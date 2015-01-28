e040b0bfd947f525f0b852bc5336b1335b1e80a6
Mikola Lysenko <mikolalysenko@gmail.com>
2015-01-28 16:04:20 -0600

    fix #1

--- robust-pnp.js
+++ robust-pnp.js
@@ -1,54 +1,101 @@
-"use strict"
-
-var orient = require("robust-orientation")
-
 module.exports = robustPointInPolygon
+
+var orient = require('robust-orientation')
 
 function robustPointInPolygon(vs, point) {
   var x = point[0]
   var y = point[1]
   var n = vs.length
   var inside = 1
-  for (var i = 0, j = n - 1; i < n; j = i++) {
+  var lim = n
+  for(var i = 0, j = n-1; i<lim; j=i++) {
     var a = vs[i]
     var b = vs[j]
     var yi = a[1]
     var yj = b[1]
-    if(yi > yj) {
-      if(yj < y && y <= yi) {
+    if(yj < yi) {
+      if(yj < y && y < yi) {
         var s = orient(a, b, point)
         if(s === 0) {
+          console.log('here1', a, b, point)
           return 0
         } else {
-          inside ^= (s > 0)|0
+          inside ^= (0 < s)|0
+        }
+      } else if(y === yi) {
+        var c = vs[(i+1)%n]
+        var yk = c[1]
+        if(yi < yk) {
+          var s = orient(a, b, point)
+          if(s === 0) {
+            return 0
+          } else {
+            inside ^= (0 < s)|0
+          }
         }
       }
-    } else if(yj > yi) {
-      if(yi <= y && y < yj) {
+    } else if(yi < yj) {
+      if(yi < y && y < yj) {
         var s = orient(a, b, point)
         if(s === 0) {
+          console.log('here2', a, b, point)
           return 0
         } else {
           inside ^= (s < 0)|0
         }
+      } else if(y === yi) {
+        var c = vs[(i+1)%n]
+        var yk = c[1]
+        if(yk < yi) {
+          var s = orient(a, b, point)
+          if(s === 0) {
+            return 0
+          } else {
+            inside ^= (s < 0)|0
+          }
+        }
       }
     } else if(y === yi) {
-      var xi = a[0]
-      var xj = b[0]
-      if(xi < xj) {
-        if(x < xi) {
-          inside ^= 1
-        } else if(x <= xj) {
-          return 0
+      var x0 = Math.min(a[0], b[0])
+      var x1 = Math.max(a[0], b[0])
+      if(i === 0) {
+        while(j>0) {
+          var k = (j+n-1)%n
+          var p = vs[k]
+          if(p[1] !== y) {
+            break
+          }
+          var px = p[0]
+          x0 = Math.min(x0, px)
+          x1 = Math.max(x1, px)
+          j = k
         }
-      } else if(xi > xj) {
-        if(x < xj) {
-          inside ^= 1
-        } else if(x <= xi) {
-          return 0
+        if(j === 0) {
+          if(x0 <= x && x <= x1) {
+            return 0
+          }
+          return 1 
         }
-      } else if(x === xi) {
+        lim = j+1
+      }
+      var y0 = vs[(j+n-1)%n][1]
+      while(i+1<lim) {
+        var p = vs[i+1]
+        if(p[1] !== y) {
+          break
+        }
+        var px = p[0]
+        x0 = Math.min(x0, px)
+        x1 = Math.max(x1, px)
+        i += 1
+      }
+      if(x0 <= x && x <= x1) {
+        console.log(x0, x, x1, y)
         return 0
+      }
+      var y1 = vs[(i+1)%n][1]
+      if(x < x0 && (y0 < y !== y1 < y)) {
+        inside ^= 1
       }
     }
   }
